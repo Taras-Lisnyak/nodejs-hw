8b9511e77e9d4d2348b64abb7e0fd3115c658600
Taras-Lisnyak <taras4lisnyak@gmail.com>
2026-04-12 23:38:27 +0300

    edit name middlewarE

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pinoHttp from 'pino-http';
-import { logger } from './middlewares/logger.js';
-import notFoundHandler  from './middlewares/notFoundHandler.js';
-import errorHandler   from './middlewares/errorHandler.js';
+import { logger } from './middleware/logger.js';
+import notFoundHandler  from './middleware/notFoundHandler.js';
+import errorHandler   from './middleware/errorHandler.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import notesRoutes from './routes/notesRoutes.js';
 
